bf3b26e006af1040bd6860f5402c028e6653de55
john doe <devops@ample.com>
2023-06-12 16:17:35 +0530

    awk tr cmd files

---
+++ 17.awk_cmd/awk.sh
@@ -0,0 +1,9 @@
+https://www.youtube.com/watch?v=ty_IgJ8-BaY&list=PL2qzCKTbjutJRE_m5uas6IrdFaNN786fH&index=16&ab_channel=VRTechnologiesForAutomation%7CByNarendraP
+
+awk has scipting also
+    NR=number for record
+
+# syntax
+ awk '{action $1}' file.txt
+ awk '{print $1}' demo.txt
+ httpd -v |awk '{print $6}'
